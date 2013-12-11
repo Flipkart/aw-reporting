cc52e97e05958c0c3230ba05220f475cada03e00
Deepti Agrawal <deeptia@flipkart.com>
2013-12-11 10:51:57 +0530

    adding build file

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd aw-report-model
+mvn clean install -DskipTests=true
+cd ../aw-reporting
+mvn clean install -DskipTests=true
+mvn deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-release-local
+mvn deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-releases-local
+#java -Xmx4G -jar target/aw-reporting-full.jar -dateRange LAST_30_DAYS -file ~/Desktop/aw-report-sample.properties
