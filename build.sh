9dc41ef5eb53713f0756bbcfd9bfa6d1cb0723f5
Deepti Agrawal <deeptia@flipkart.com>
2013-12-11 18:58:03 +0530

    pom changes to create uber jar

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 cd aw-report-model
 mvn clean install -DskipTests=true
+mvn deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-release-local
+mvn deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-releases-local
 cd ../aw-reporting
+mvn clean install -DskipTests=true
+mvn deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-release-local
+mvn deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-releases-local
+cd examples/kratubackend
 mvn clean install -DskipTests=true
 mvn deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-release-local
 mvn deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-releases-local
