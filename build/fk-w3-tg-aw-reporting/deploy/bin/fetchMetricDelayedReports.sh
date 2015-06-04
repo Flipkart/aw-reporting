64f623b9739c2c59751be245553acdc64f1108fa
Vignesh Raghuraman <mit.vig@gmail.com>
2015-06-05 00:49:13 +0530

    Fixing port issues of diff download scripts

--- build/fk-w3-tg-aw-reporting/deploy/bin/fetchMetricDelayedReports.sh
+++ build/fk-w3-tg-aw-reporting/deploy/bin/fetchMetricDelayedReports.sh
@@ -22,6 +22,6 @@ JAVA_OPTS="$JAVA_OPTS -Xmn512M -Xms2048M -Xmx4096M -XX:PermSize=256M -XX:MaxPerm
 JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8"
 
 ##JMX
-JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=8896 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote=true"
+JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=8889 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote=true"
 
 exec java -jar $JAVA_OPTS $JAR_FILE -startDate `date --date="-2 days" +"%Y%m%d"` -endDate `date --date="-2 days" +"%Y%m%d"` -file $CONF_FILE -accountIdsFile $ACCOUNTS_FILE
