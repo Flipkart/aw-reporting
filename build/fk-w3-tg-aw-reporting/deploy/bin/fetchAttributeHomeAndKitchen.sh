f8b25ea68733ed5fd07435a33246cde8df7ef85e
Vignesh Raghuraman <mit.vig@gmail.com>
2015-07-30 20:13:28 +0530

    Changing port for Home & Kitchen a/c download

--- build/fk-w3-tg-aw-reporting/deploy/bin/fetchAttributeHomeAndKitchen.sh
+++ build/fk-w3-tg-aw-reporting/deploy/bin/fetchAttributeHomeAndKitchen.sh
@@ -20,6 +20,6 @@ JAVA_OPTS="$JAVA_OPTS -Xmn512M -Xms2048M -Xmx4096M -XX:PermSize=256M -XX:MaxPerm
 JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8"
 
 ##JMX
-JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=8898 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote=true"
+JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=8900 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote=true"
 
 exec java -jar $JAVA_OPTS $JAR_FILE -dateRange YESTERDAY -file $CONF_FILE -accountIdsFile $ACCOUNTS_FILE
